3938bb6925931a77d99995a0368f0286ae4ed950
shubhangi013 <choudharyshubhangi13@gmail.com>
2021-12-25 22:17:40 +0530

    improved ui

--- src/components/DownloadFile.tsx
+++ src/components/DownloadFile.tsx
@@ -1,7 +1,7 @@
 const DownloadFile = ({downloadPageLink}) => {
   return (
     <div className="p-1">
-      <h1 className="my-2 text-lg font-medium">SHUBHANGI BEST</h1>
+      <h1 className="my-2 text-lg font-medium">Use the below given URL and share your file!</h1>
       <div className="flex space-x-3">
         <span className="break-all"> {downloadPageLink} </span>
         <img src="/images/copy.png" alt="copy" className="w-8 h-8 object-contain cursor-pointer"
